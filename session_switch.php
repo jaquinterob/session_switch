743112e34dc9dbb2c36b636a5683a85755d7e450
John Alexander Quintero <quinteroberrio@gmail.com>
2019-01-07 19:49:44 -0500

    se generan las variables de sesión automaticamente, y ignorando los campos que se consideren necesarios

--- session_switch.php
+++ session_switch.php
@@ -4,12 +4,20 @@ include('config/conexion.php');
 
 $sql="SHOW COLUMNS FROM switch.usuarios WHERE `Field` NOT IN ('usuario');";
 $res=$connect->query($sql);
+$row_usuario=obtener_row_usuario('john.quintero');
 while ($row=$res->fetch_assoc()) {
-  
+$_SESSION[$row['Field']]=$row_usuario[$row['Field']];
 }
 print_r( $_SESSION );
 
-
+function obtener_row_usuario($usuario){
+  global $connect;
+  $sql="SELECT * FROM switch.usuarios WHERE usuario='".$usuario."'";
+  $res=$connect->query($sql);
+  if ($res->num_rows>0) {
+      return $row=$res->fetch_assoc();
+  }
+}
 
 
 
