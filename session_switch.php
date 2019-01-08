c1bc57fc90db7bcbe6a52cdd174087388fad2586
John Alexander Quintero <quinteroberrio@gmail.com>
2019-01-07 19:30:19 -0500

    commit inicial

--- session_switch.php
+++ session_switch.php
@@ -1,29 +1,13 @@
 <?php
 session_start();
 include('config/conexion.php');
-// $_SESSION['mensajeriapp']=1;
-// $_SESSION['te_informo']=0;
-// $_SESSION['mda']=0;
-$row_usuario=obtener_row_usuario('john.quintero');
 
-$sql="SELECT * FROM switch.session_switch";
+$sql="SHOW COLUMNS FROM switch.usuarios WHERE `Field` NOT IN ('usuario');";
 $res=$connect->query($sql);
-while ($row_session=$res->fetch_assoc()) {
-$_SESSION[$row_session['aplicativo']]=$row_usuario[$row_session['switch']];
+while ($row=$res->fetch_assoc()) {
+  
 }
-
-echo 'mda= '.$_SESSION['mda'].'<br>';
-echo 'mensajeriapp= '.$_SESSION['mensajeriapp'].'<br>';
-echo 'te_informo= '.$_SESSION['te_informo'];
-
-function obtener_row_usuario($usuario){
-  global $connect;
-  $sql="SELECT * FROM switch.usuarios WHERE usuario='".$usuario."'";
-  $res=$connect->query($sql);
-  if ($res->num_rows>0) {
-      return $row=$res->fetch_assoc();
-  }
-}
+print_r( $_SESSION );
 
 
 
